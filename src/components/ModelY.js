07d5630332f59b16e9f3cee17c33020a9bcb4cd8
jsm8109jsm <jsm8109jsm@gmail.com>
2022-06-30 15:32:37 +0900

    add: 인테리어 버튼 완성

--- src/components/ModelY.js
+++ src/components/ModelY.js
@@ -20,7 +20,16 @@ export default function ModelY() {
 
     const [border19, setBorder19] = useState('3px solid #3457b1');
     const [border20, setBorder20] = useState('0px solid');
-    
+
+
+    const [borderBlack, setBorderBlack] = useState('3px solid #3457b1');
+    const [borderWhite, setBorderWhite] = useState('0px solid');
+
+    const [interiorName, setInteriorName] = useState('All Black');
+    const [interiorPrice, setInteriorPrice] = useState('포함');
+
+    const [interior, setInterior] = useState('black');
+
     useEffect(() => {
         setDistance(511);
         setSpeed(217);
@@ -50,17 +59,6 @@ export default function ModelY() {
         setWheel('');
     }
 
-    // const carImgName = (newColor) => {
-    //     if (isPer) {
-    //         setCarImg('basic');
-    //     }
-    //     else {
-    //         setCarImg('perform');
-    //     }
-    //     setCarImg(prev => prev += '--' + newColor);
-    // }
-
-    
     function set19() {
         if (wheelName === '19인치 제미니 휠') return;
         setWheelName('19인치 제미니 휠');
@@ -79,11 +77,28 @@ export default function ModelY() {
         setWheel('--20');
     }
 
+    function setInteriorBtn(interiorColor) {
+        if (interiorColor === 'black') {
+            setInteriorName('All Black');
+            setInteriorPrice('포함');
+            setBorderBlack("3px solid #3457b1");
+            setBorderWhite("0px solid");
+            setInterior("black");
+        }
+        else {
+            setInteriorName('Black & White');
+            setInteriorPrice('₩1,286,000');
+            setBorderBlack("0px solid");
+            setBorderWhite("3px solid #3457b1");
+            setInterior("white");
+        }
+    }
+
     return (
         <div>
             <OrderHeader />
             <div className="content">
-                <ModelYImg form={isPer} color={color} wheel={wheel}/>
+                <ModelYImg form={isPer} color={color} wheel={wheel} interior={interior} />
                 <main className="car--content">
                     <h1 className="car--name">Model Y</h1>
                     <div className="main">
@@ -140,24 +155,44 @@ export default function ModelY() {
                         </div>
                         <span className="color--name">{colorName}</span><span className="color--price">{colorPrice}</span>
                     </div>
-                    <div className="wheel--div">
-                        <div className="color">
-                            <h4 className="detail--title">휠</h4>
-                            {
-                                isPer ? <div id="wheelBtn">
-                                    <button className="color--btn" id="setWheelInfo19" style={{ border: border19 }} onClick={set19}>
-                                        <img src="../images/19.png" className="color--img" alt="바퀴사진" />
-                                    </button>
-                                    <button className="color--btn" id="setWheelInfo20" style={{ border: border20 }} onClick={set20}>
-                                        <img src="../images/20.png" className="color--img" alt="바퀴사진" />
-                                    </button>
-                                </div> :
-                                    <div id="wheelBtn">
-                                        <button className="color--btn" style={{ border: '3px solid #3457b1' }}><img src="../images/21.png" className="color--img" alt="바퀴사진" /></button>
-                                    </div>
-                            }
-                            <span className="color--name">{wheelName}</span><span className="color--price">{wheelPrice}</span>
+                    <div className="wheel">
+                        <h4 className="detail--title">휠</h4>
+                        {
+                            isPer ? <div className="wheel--btn--div">
+                                <button className="wheel--btn" id="setWheelInfo19" style={{ border: border19 }} onClick={set19}>
+                                    <img src="../images/19.png" className="wheel--img" alt="바퀴사진" />
+                                </button>
+                                <button className="wheel--btn" id="setWheelInfo20" style={{ border: border20 }} onClick={set20}>
+                                    <img src="../images/20.png" className="wheel--img" alt="바퀴사진" />
+                                </button>
+                            </div> :
+                                <div className="wheel--btn--div">
+                                    <button className="wheel--btn" style={{ border: '3px solid #3457b1' }}><img src="../images/21.png" className="color--img" alt="바퀴사진" /></button>
+                                </div>
+                        }
+                        <span className="wheel--name">{wheelName}</span><span className="wheel--price">{wheelPrice}</span>
+                    </div>
+                    <div className="interior">
+                        <h4 className="detail--title">인테리어</h4>
+                        <div className="interior--btn--div">
+                            <button className="interior--btn" style={{ border: borderBlack }} onClick={() => setInteriorBtn('black')}>
+                                <img src="../images/interior_black.png" className="interior--img" alt="인테리어사진" />
+                            </button>
+                            <button className="interior--btn" style={{ border: borderWhite }} onClick={() => setInteriorBtn('white')}>
+                                <img src="../images/interior_white.png" className="interior--img" alt="인테리어사진" />
+                            </button>
                         </div>
+                        <span className="interior--name">{interiorName}</span><span className="interior--price">{interiorPrice}</span>
+                        <h6 className="interior--subtitle">시트 구성</h6>
+                        <div className="wheel--btn--div">
+                            <button className="interior--btn" style={{ border: '3px solid #3457b1' }}><span className="interior--img">5</span></button>
+                        </div>
+                        <span className="interior--name">5인승</span><span className="interior--price">포함</span>
+                        <ul className="interior--ul">
+                            <li className="interior--li">등받이 조절 가능한 2열 좌석</li>
+                            <li className="interior--li">최대 수납을 위한 완전 접이식 2열 좌석</li>
+                            <li className="interior--li">트렁크 내부에 위치한 전동식 좌석 폴딩 버튼</li>
+                        </ul>
                     </div>
                 </main>
             </div>
--- src/components/ModelYImg.js
+++ src/components/ModelYImg.js
@@ -1,13 +1,23 @@
-import React from "react";
+import {React, useState, useEffect} from "react";
 
 export default function ModelYImg(props){
+    const [scrollPosition, setScrollPosition] = useState(0);
+    const updateScroll = () => {
+        setScrollPosition(window.scrollY || document.documentElement.scrollTop);
+    }
+    useEffect(()=>{
+        window.addEventListener('scroll', updateScroll);
+    });
+
+    console.log(props.interior);
+
     let form = props.form?'basic':'perform';
     console.log(form + '--' + props.color + props.wheel);
     let carImg = `../images/${form}--${props.color}${props.wheel}.png`
 
     return(
         <div className="img--div">
-            <img src={carImg}  alt="차 사진"></img>
+            <img src={scrollPosition < 600 ? carImg : `../images/interior--car--${props.interior}.jpg`}  alt="차 사진"></img>
         </div>
     )
 }
