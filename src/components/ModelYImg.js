d8699f0f20b241ca03bdd134a05748e73c69b656
jsm8109jsm <jsm8109jsm@gmail.com>
2022-07-05 08:08:23 +0900

    add: 진짜 마무리

--- src/components/ModelYImg.js
+++ src/components/ModelYImg.js
@@ -15,7 +15,13 @@ export default function ModelYImg(props){
     return(
         <div className="img--div">
             {
+                props.form?
                 scrollPosition > 1500 && scrollPosition < 2500?
+                    <video src="https://www.tesla.com/ns_videos/model3/autopilot/navigate-on-autopilot.mp4" className="img" autoPlay={true} muted={true} loop={true}></video>
+                :
+                    <img src={scrollPosition < 600 || scrollPosition > 2500? carImg : `../images/interior--car--${props.interior}.jpg`} alt="차 사진"></img>
+                    :
+                    scrollPosition > 2000 && scrollPosition < 3000?
                     <video src="https://www.tesla.com/ns_videos/model3/autopilot/navigate-on-autopilot.mp4" className="img" autoPlay={true} muted={true} loop={true}></video>
                 :
                     <img src={scrollPosition < 600 || scrollPosition > 2500? carImg : `../images/interior--car--${props.interior}.jpg`} alt="차 사진"></img>
